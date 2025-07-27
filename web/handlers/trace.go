6579a1a89a6c2dcebe09cb52f4896a87b6abbc1d
sharmaann <shakerian.arman@yahoo.com>
2025-07-27 08:41:33 +0330

    feat: add logging in /var/log/my_traceroute.log

--- web/handlers/trace.go
+++ web/handlers/trace.go
@@ -7,7 +7,37 @@ import (
 	"trg/pkg/cache.go"
 
 	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+	"encoding/json"
 )
+
+func add_log(data []*services.Result){
+	file, err := os.OpenFile("/var/log/my_traceroute.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if (err != nil){
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	logEntry := struct {
+		Timestamp time.Time          `json:"timestamp"`
+		Results   []*services.Result `json:"results"`
+	}{
+		Timestamp: time.Now(),
+		Results:   data,
+	}
+
+	jsonData, err := json.Marshal(logEntry)
+	if err != nil {
+		log.Printf("Failed to marshal results to JSON: %v", err)
+		return
+	}
+
+	// Write to log file with newline
+	if _, err := file.Write(append(jsonData, '\n')); err != nil {
+		log.Printf("Failed to write to log file: %v", err)
+	}
+}
 
 func Trace(cc cache.Provider) fiber.Handler {
 	return func(c *fiber.Ctx) error {
@@ -17,6 +47,8 @@ func Trace(cc cache.Provider) fiber.Handler {
 		oc := cache.NewObjectCacher[[]*services.Result](cc, cache.SerializationTypeJSON)
 
 		oc.Set(c.UserContext(), fmt.Sprintf("%s:%s", c.Request().String(), c.Context().Time()), time.Hour*1, result)
+		
+		add_log(result)
 
 		return c.Status(200).JSON(result)
 	}
